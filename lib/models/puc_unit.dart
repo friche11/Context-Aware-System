5253deae1f000c67accfe19ae3742e3f59b39337
friche11 <vicfriche@gmail.com>
2024-12-03 15:21:25 -0300

    feat: model que representa a unidade da Puc Minas

---
+++ lib/models/puc_unit.dart
@@ -0,0 +1,13 @@
+class PucUnit {
+  final String name;
+  final String address;
+  final double latitude;
+  final double longitude;
+
+  PucUnit({
+    required this.name,
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+  });
+}
